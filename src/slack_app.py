cf15611e83bba147f48651863d0f4ac36c08823d
Janglish <j4ngl1sh@gmail.com>
2025-02-13 22:53:42 +0900

    modify nltk

--- src/slack_app.py
+++ src/slack_app.py
@@ -9,7 +9,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain.memory import ChatMessageHistory
 
 load_dotenv()
-nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 index = create_index()
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
